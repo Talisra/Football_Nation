821b1640e40d86bc81897e8f50bda5d4376d7719
ISeePlusPlus <yigaldiker@gmail.com>
2019-09-25 21:31:27 +0300

    did something

---
+++ Football_Nation/Person.cpp
@@ -0,0 +1,17 @@
+#include "person.h"
+
+Person::Person(const char* name, int age, const char* nationality)
+{
+	name = new char[strlen(name) + 1];
+	strcpy(this->name, name);
+	this->age = age;
+	nationality = new char[strlen(nationality) + 1];
+	strcpy(this->nationality, nationality);
+}
+
+void Person::show() const 
+{
+
+}
+
+//ADD GETTERS AND SETTERS
