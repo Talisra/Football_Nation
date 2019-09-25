0b33335d95c2ce0178b96e219f3de472f66754f1
Talisra <talisraeli.t@gmail.com>
2019-09-25 21:30:09 +0300

    match implementation

---
+++ Football_Nation/Match.cpp
@@ -0,0 +1,40 @@
+#include "match.h"
+
+Match::Match(Team* homeTeam, Team* awayTeam, Referee* referee) : homeTeam(homeTeam), awayTeam(awayTeam), referee(referee)
+{
+	playMatch();
+}
+
+void Match::playMatch()
+{
+
+}
+
+Team* Match::getWinnerTeam() const
+{
+	if (result[0] != result[1])
+		result[0] > result[1] ? homeTeam : awayTeam;
+	else
+		return nullptr;
+}
+
+
+Team* Match::getHomeTeam() const
+{
+	return homeTeam;
+}
+
+Team* Match::getAwayTeam() const
+{
+	return awayTeam;
+}
+
+Referee* Match::getReferee() const
+{
+	return referee;
+}
+
+int Match::getResult() const //TODO 
+{
+	return 0;
+}
