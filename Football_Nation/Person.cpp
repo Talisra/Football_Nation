b991f3bfbf4d92d7ef12bb0d4603ec37b1e4c774
ISeePlusPlus <yigaldiker@gmail.com>
2019-09-27 15:57:17 +0300

    finishing off person.cpp Should ask if changes are allowed to h file

--- Football_Nation/Person.cpp
+++ Football_Nation/Person.cpp
@@ -1,12 +1,18 @@
 #include "person.h"
 
-Person::Person(const char* name, int age, const char* nationality)
+Person::Person(const char* name, int age=17, const char* nationality)
 {
-	name = new char[strlen(name) + 1];
+	this->name = new char[strlen(name) + 1];
+	this->nationality = new char[strlen(nationality) + 1];
 	strcpy(this->name, name);
 	this->age = age;
-	nationality = new char[strlen(nationality) + 1];
 	strcpy(this->nationality, nationality);
+}
+
+Person::~Person() 
+{
+	delete []name;
+	delete []nationality;
 }
 
 void Person::show() const
@@ -14,4 +20,33 @@ void Person::show() const
 
 }
 
-//ADD GETTERS AND SETTERS
+//Getters
+
+char* Person::getName() const { return name; }
+int Person::getAge() const { return age; }
+char* Person::getNationality() const{ return nationality; }
+
+//Setters
+
+void Person::setName(const char* newName) 
+{
+	delete []name;
+	name = new char[strlen(newName) + 1];
+	strcpy(name, newName);
+}
+bool Person::setAge(int newAge)
+{
+	if (newAge < 17 || newAge > 75)
+		return false;
+	age = newAge;
+	return true;
+}
+void Person::setNationality(const char* newNationality)
+{
+	delete []nationality;
+	nationality = new char[strlen(newNationality) + 1];
+	strcpy(nationality, newNationality);
+}
+
+
+
--- Football_Nation/person.h
+++ Football_Nation/person.h
@@ -1,18 +1,28 @@
 #ifndef __PERSON_H
 #define __PERSON_H
 #include <iostream>
-const int NAME_SIZE = 30;
+//const int NAME_SIZE = 30;
 class Person
 {
 protected:
-	char name[NAME_SIZE];
+	char* name;//[NAME_SIZE];
 	int age;
-	char nationality[NAME_SIZE];
-	Person(const Person& other);
+	char* nationality; //[NAME_SIZE] ;
+	Person(const Person& other) = delete;
+	const Person& operator=(const Person& other) = delete;
+	~Person();
+
 public:
 	Person(const char * name, int age, const char* nationality);
 	void show() const;
 
+	char* getName() const;
+	int getAge() const;
+	char* getNationality() const;
+
+	void setName(const char* newName);
+	bool setAge(int newAge);
+	void setNationality(const char* newNationality);
 };
 
 #endif //__PERSON_H
