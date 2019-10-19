792a103e433d82bcbdfd72d8024ca50a86b632c5
ISeePlusPlus <yigaldiker@gmail.com>
2019-10-19 16:32:38 +0300

    << of match and fixture

--- Football_Nation/Fixture.cpp
+++ Football_Nation/Fixture.cpp
@@ -8,9 +8,8 @@ Fixture::Fixture(int gamesInFixture, int number, Match** matches):
 
 ostream& operator<<(ostream& os, const Fixture& fixture)
 {
-
-	for (Match match : fixture.matches)
-		os << "Home Team: " + match.getHomeTeam + " vs " + "Away Team: " + match.getAwayTeam + ",result is: " + match.getResult + "\n";
+	for (int i = 0; i < fixture.gamesInFixture; i++)
+		os << *fixture.matches[i];
 	return os;
 }
 
