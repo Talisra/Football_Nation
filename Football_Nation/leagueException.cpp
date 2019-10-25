39062ebf823e6639c5910b7a1e281d32304f0b95
ISeePlusPlus <yigaldiker@gmail.com>
2019-10-25 17:08:03 +0300

    big l

--- Football_Nation/leagueException.cpp
+++ Football_Nation/leagueException.cpp
@@ -10,7 +10,7 @@ void NoRefferesException::show() const
 	std::cout << exceptionReason << ", please add referees to the league" << std::endl;
 }
 
-void leagueException::show() const
+void LeagueException::show() const
 {
 	std::cout << exceptionReason << std::endl;
 }
